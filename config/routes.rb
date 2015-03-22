2e8cfb2237616e8d2f06f3f056c77ac911676a72
PaulYoum <youmster@gmail.com>
2015-03-22 10:00:10 -0700

    added routes to tracking

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  
+  scope :match, controller: :match do 
+    get "", action: :index
+  end
 
   # You can have the root of your site routed with "root"
   root 'landing#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
